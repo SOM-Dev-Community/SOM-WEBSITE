80f46d8afd1752119bf729fe23d01ac84630a1ac
ProsperMadojemu <prospermadojemu00@gmail.com>
2026-04-18 17:05:36 +0100

    Add image quality settings to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   // enable Cache Components so `'use cache'` directives take effect
   cacheComponents: true,
   images: {
+    qualities:[75, 90, 100],
     remotePatterns: [
       {
         protocol: "https",
